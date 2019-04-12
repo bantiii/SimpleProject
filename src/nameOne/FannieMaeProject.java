5967202886399e92043326abc6601ea3b1d09a9b
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-12 13:47:08 -0700

    we added some comment Man! go check that out stupid indian!!

--- src/nameOne/FannieMaeProject.java
+++ src/nameOne/FannieMaeProject.java
@@ -9,6 +9,10 @@ for(String one:oneName){
 	System.out.println("the value of the first man:  "+one);
 	System.out.println("===================================================");
 	//oh yeah now I get it !!! shit this easy ===now I know  man!!
+	
+	System.out.println("we gonna get that job and make  some more money==");
+	System.out.println("Imma call and tell my mum that I'm working profesional job");
+	System.out.println("On Sunday and Saturday I will go to Kaldis to show up and also get beautiful girls");
 }
 	}
 
