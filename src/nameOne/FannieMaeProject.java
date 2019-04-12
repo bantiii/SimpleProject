a59e1aaa8b04fb5a3fdae7cdb5af0ea64ecfca79
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-12 13:38:52 -0700

    added one comment

--- src/nameOne/FannieMaeProject.java
+++ src/nameOne/FannieMaeProject.java
@@ -8,6 +8,7 @@ for(String one:oneName){
 	System.out.println("this is gonna be the first comment ever==");
 	System.out.println("the value of the first man:  "+one);
 	System.out.println("===================================================");
+	//oh yeah now I get it !!! shit this easy ===now I know  man!!
 }
 	}
 
