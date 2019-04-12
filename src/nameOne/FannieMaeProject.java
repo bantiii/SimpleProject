35915cfcbe9039247899f1ac343542a9c868d75f
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-12 13:31:32 -0700

    First One

---
+++ src/nameOne/FannieMaeProject.java
@@ -0,0 +1,12 @@
+package nameOne;
+
+public class FannieMaeProject {
+
+	public static void main(String[] args) {
+String [] oneName = {"Bantamelak", "Tafete", "Ayalew"};
+for(String one:oneName){
+	System.out.println("the value of the first man:  "+one);
+}
+	}
+
+}
