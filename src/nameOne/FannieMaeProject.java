8cb65249e79e826bc63f62074785dd210748b844
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-12 13:36:10 -0700

    one massege added

--- src/nameOne/FannieMaeProject.java
+++ src/nameOne/FannieMaeProject.java
@@ -5,7 +5,9 @@ public class FannieMaeProject {
 	public static void main(String[] args) {
 String [] oneName = {"Bantamelak", "Tafete", "Ayalew"};
 for(String one:oneName){
+	System.out.println("this is gonna be the first comment ever==");
 	System.out.println("the value of the first man:  "+one);
+	System.out.println("===================================================");
 }
 	}
 
